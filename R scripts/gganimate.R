f843de7ef5d95d6104f7755d74620bf30e6dbe37
cheryltky <ctkayin@hotmail.com>
2020-08-21 15:01:51 +1000

    update to include dashboard in repo

--- R scripts/gganimate.R
+++ R scripts/gganimate.R
@@ -1,84 +1,105 @@
-library(ggplot2)
+---
+title: "gganimate"
+date: "15 April 2020"
+output: html_document
+---
+
+###Application of Covid19 dataset to gganimate
+
+```{r load_libraries, echo=FALSE}
+# coronavirus evolution of cases over time using the Johns Hopkins data set
+# 1.03.2020, protzetter@bluewin.ch, https://www.linkedin.com/in/rotzetter/
+
 library(gganimate)
-theme_set(theme_bw())
-
-library(gapminder)
-head(gapminder)
-
-#static plot
-p <- ggplot(
-  gapminder,
-  aes(x = gdpPercap, y = lifeExp, size = pop, color = 
-        country)) +
-  geom_point(show.legend = FALSE, alpha = 0.7) +
-  scale_color_viridis_d() +
-  scale_size(range = c(2,12)) +
-  scale_x_log10() +
-  labs(x = "GDP per capita", y = "Life expectancy")
-p
-
-#time transition plot
-
-p + transition_time(year) +
-  labs(title = "Year:{frame_time}")
-
-#create facets by continent
-p + facet_wrap(~ continent) +
-  transition_time(year)+
-  labs(title = "Year:{frame_time}")
-
-#staticplot in line graphs
-
-p <- ggplot(
-  airquality,
-  aes(Day,Temp,group = Month, color = factor(Month))) +
-  geom_line() +
-  scale_color_viridis_d() +
-  labs ( x = "Day of Month", y = "Temperature") +
-  theme(legend.position = "top")
-
-p
-
-#Let data gradually appear
-## Reveal by day ( x-axis)
-
-p + transition_reveal(Day)
-
-#show points
-
-p +
-  geom_point() +
-  transition_reveal(Day)
-
-#keeping points on a graph
-
-p +
-  geom_point(aes(group = seq_along(Day)))+
-  transition_reveal(Day)
-
-#Transition between diff stages of the data
 library(dplyr)
-mean.temp <- airquality %>% 
-  group_by(Month) %>% 
-  summarise(Temp = mean(Temp))
-mean.temp
+library(tidyverse)
+library(reshape2)
+library(ggthemes)
+library(gifski)
+library(av)
+install.packages("av")
 
-#create a bar plot of mean temperature
+# load data from Johns Hopkins github
 
-p <- ggplot(mean.temp, aes(Month, Temp, fill = Temp)) +
-geom_col()+
-  scale_fill_distiller(palette = "Reds", direction = 1)+
-  theme_minimal()+
-  theme(
-    panel.grid = element_blank(),
-    panel.grid.major.y = element_line(color = "white"),
-    panel.ontop = TRUE)
-p
+confirmedCases= read.csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+deathCases= read.csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
+recoveredCases= read.csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv')
 
-#transition states
 
-p + transition_states(Month, wrap = FALSE)+
-  shadow_mark()+
-#enter grow and fdae
-enter_grow()+
-  enter_fade()
+# set system locale for date conversion purpose
+Sys.setlocale("LC_TIME", "English")
+
+#convert data sets into columns and remove unwanted columns
+confirmedCases<-confirmedCases%>%select(-c(Lat,Long))%>%melt(id=c('Country/Region','Province/State'))
+confirmedCases<-confirmedCases%>%group_by(`Country/Region`,variable)%>%summarise(Confirmed=sum(value))
+
+deathCases<-deathCases%>%select(-c(Lat,Long))%>%melt(id=c('Country/Region','Province/State'))
+deathCases<-deathCases%>%group_by(`Country/Region`,variable)%>%summarise(Deaths=sum(value))
+
+recoveredCases<-recoveredCases%>%select(-c(Lat,Long))%>%melt(id=c('Country/Region','Province/State'))
+recoveredCases<-recoveredCases%>%group_by(`Country/Region`,variable)%>%summarise(Recovered=sum(value))
+
+
+# rename table columns
+colnames(confirmedCases)<-c("Country","Date","Confirmed")
+colnames(deathCases)<-c("Country","Date","Death")
+colnames(recoveredCases)<-c("Country","Date","Recovered")
+
+# merge all atbles together
+
+mergedCases<-merge(confirmedCases,deathCases, by.y=c("Country","Date"))
+mergedCases<-merge(mergedCases,recoveredCases, by.y=c("Country","Date"))
+
+# convert factors to date format
+
+mergedCases$Date<-as.Date(mergedCases$Date,"%m/%d/%y")
+
+# summarize cases by date
+df1<-mergedCases %>% group_by(Date) %>% summarise_at(c("Confirmed","Recovered","Death"),sum)
+
+dff<-as.data.frame(rbind( c(0,0,0),apply(df1[-1] , 2 , diff )))
+colnames(dff)<-c("New","New recovered","New Death")
+
+
+df1<-cbind(df1,dff)
+
+# stack columns together and add state columns to each case
+df2 <- data.frame(Date=rep(df1$Date, 4), 
+                  cases=c(df1$Confirmed, df1$Death,df1$Recovered, df1$New), 
+                  State=rep(c("Confirmed","Deaths", "Recovered", "New"), each=nrow(df1)))
+
+
+# retrieve last update date for title
+lastDate<-max(df1$Date)
+
+# define plot object
+p<-ggplot(df2, aes(x=Date, y=cases, group=State, color=State)) +
+  geom_line() +
+  geom_segment(aes(xend=max(Date), yend = cases), linetype=2, colour='blue') +
+  geom_point(size = 3) + 
+  geom_text(aes(x = max(Date)+.1, label = sprintf("%5.0f", cases)), hjust=-0.5) +
+  transition_reveal(Date) + 
+  view_follow(fixed_y = TRUE)+
+  coord_cartesian(clip = 'off') + 
+  xlab("Day") +
+  ylab("Number of cumulative cases") + ggtitle(paste("Evolution of cumulative cases over time as of ",lastDate)) +
+  enter_drift(x_mod = -1) + exit_drift(x_mod = 1) +
+  theme_classic() +
+  theme(legend.position = c(0.2, 0.8))+
+  theme(panel.border = element_blank(),
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  axis.line = element_line(colour = "black"),
+  plot.margin = margin(5.5, 40, 5.5, 5.5))
+```
+
+```{r animatedgiffile, echo=FALSE}
+
+# create animation gif file
+animate(p, fps=5,renderer = gifski_renderer("virusevolution.gif"), end_pause = 10)
+
+#create mp4 file
+animate(p, fps=5,renderer=av_renderer('virusevolution.mp4'))
+```
+
+
